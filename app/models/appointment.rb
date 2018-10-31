02d9606716119fdf30879f28da2a5e8b9ab0c530
Pamela Frieze <pfrieze@gmail.com>
2018-10-31 00:53:49 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
 
-  def datetime_convert
+  def datetime_convert(d)
     self.appointment_datetime.strftime(%A, %d %b %Y %l:%M %p)
   end
 end
